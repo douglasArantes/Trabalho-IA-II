70320e371872b8a991d4419ad4dfb6ae74e7f696
douglasArantes <douglas_arantes@outlook.com>
2015-06-02 00:28:46 -0400

    Remodelagem

--- classificador/modelos/BaseClassifier.py
+++ classificador/modelos/BaseClassifier.py
@@ -17,19 +17,28 @@ class BaseClassifier(object):
         features, labels = self.extractFeaturesAndLabels()
         scores = crossV.cross_val_score(model, features, labels, cv=10, scoring='accuracy')
 
-        print("Accuracy of 10 folds:")
+        print("Acurácia dos 10 folds:")
         for score in scores:
             print(score)
-        print("Standard Deviation of Accuracy %s" % scores.std())
+        print("Média das Acurácias %s" % scores.mean())
+        print("Desvio Padrão das Acurácias %s" % scores.std())
 
     def printAccruraciesAndStds(self):
         decisionTreeModel = DecisionTreeClassifier()
-        knnModel = KNeighborsClassifier()
+
+        knnModel1 = KNeighborsClassifier(n_neighbors=1)
+        knnModel3 = KNeighborsClassifier(n_neighbors=3)
+        knnModel5 = KNeighborsClassifier(n_neighbors=5)
+
         naiveBayesModel = GaussianNB()
 
         print("Decision Tree Model")
         self.accuracyAndStd(decisionTreeModel)
-        print("\nKNN Model")
-        self.accuracyAndStd(knnModel)
+        print("\nKNN Model K = 1")
+        self.accuracyAndStd(knnModel1)
+        print("\nKNN Model K = 3")
+        self.accuracyAndStd(knnModel3)
+        print("\nKNN Model K = 5")
+        self.accuracyAndStd(knnModel5)
         print("\nNaive Bayes Model")
         self.accuracyAndStd(naiveBayesModel)
