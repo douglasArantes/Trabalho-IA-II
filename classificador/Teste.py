e81b53aea7db6caef02592f976506da02d5bbec9
douglasArantes <douglas_arantes@outlook.com>
2015-06-03 15:43:47 -0400

    Salvando saídas em aquivo (output.txt)

--- classificador/Teste.py
+++ classificador/Teste.py
@@ -1,3 +1,6 @@
+import os
+import codecs
+
 from classificador.modelos.IrisClassifier import IrisClassifier
 from classificador.modelos.DiabetesClassifier import DiabetesClassifier
 from classificador.modelos.BalanceScaleClassifier import BalanceScaleClassifier
@@ -10,44 +13,59 @@ from classificador.modelos.ImageSegmentationClassifier import ImageSegmentationC
 from classificador.modelos.WineClassifier import WineClassifier
 
 class Teste:
+
+    outputFile = os.path.join(os.getcwd(), "output.txt")
+    fileWriter = codecs.open(outputFile, 'w', "utf-8")
+
+
     if __name__ == "__main__":
 
+        fileWriter.write("-------- Dataset Iris --------")
         print("-------- Dataset Iris --------\n")
-        IrisClassifier().printAccruraciesAndStds()
+        IrisClassifier().printAccruraciesAndStds(fileWriter)
         print()
 
+        fileWriter.write("-------- Dataset Prima Indians Diabetes --------")
         print("-------- Dataset Prima Indians Diabetes --------\n")
-        DiabetesClassifier().printAccruraciesAndStds()
+        DiabetesClassifier().printAccruraciesAndStds(fileWriter)
         print()
 
+        fileWriter.write("-------- Dataset Balance Scale --------")
         print("-------- Dataset Balance Scale --------\n")
-        BalanceScaleClassifier().printAccruraciesAndStds()
+        BalanceScaleClassifier().printAccruraciesAndStds(fileWriter)
         print()
 
+        fileWriter.write("-------- Dataset Sonar All --------")
         print("-------- Dataset Sonar All --------\n")
-        SonarClassifier().printAccruraciesAndStds()
+        SonarClassifier().printAccruraciesAndStds(fileWriter)
         print()
 
+        fileWriter.write("-------- Dataset Glass  --------")
         print("-------- Dataset Glass  --------\n")
-        GlassClassifier().printAccruraciesAndStds()
+        GlassClassifier().printAccruraciesAndStds(fileWriter)
         print()
 
+        fileWriter.write("-------- Dataset Haberman  --------")
         print("-------- Dataset Haberman  --------\n")
-        HabermanClassifier().printAccruraciesAndStds()
+        HabermanClassifier().printAccruraciesAndStds(fileWriter)
         print()
 
+        fileWriter.write("-------- Dataset Ionsphere --------")
         print("-------- Dataset Ionsphere --------\n")
-        IonosphereClassifier().printAccruraciesAndStds()
+        IonosphereClassifier().printAccruraciesAndStds(fileWriter)
         print()
 
+        fileWriter.write("-------- Dataset Spams --------")
         print("-------- Dataset Spams --------\n")
-        SpamClassifier().printAccruraciesAndStds()
+        SpamClassifier().printAccruraciesAndStds(fileWriter)
         print()
 
+        fileWriter.write("-------- Dataset Image Segmentation --------")
         print("-------- Dataset Image Segmentation --------\n")
-        ImageSegmentationClassifier().printAccruraciesAndStds()
+        ImageSegmentationClassifier().printAccruraciesAndStds(fileWriter)
         print()
 
+        fileWriter.write("-------- Dataset Wine --------")
         print("-------- Dataset Wine --------\n")
-        WineClassifier().printAccruraciesAndStds()
+        WineClassifier().printAccruraciesAndStds(fileWriter)
         print()
--- classificador/modelos/BaseClassifier.py
+++ classificador/modelos/BaseClassifier.py
@@ -1,3 +1,5 @@
+import os
+
 from sklearn import cross_validation as crossV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -13,32 +15,52 @@ class BaseClassifier(object):
     def extractFeaturesAndLabels(self):
         raise NotImplementedError
 
-    def accuracyAndStd(self, model):
+    def accuracyAndStd(self, model, fileWriter):
         features, labels = self.extractFeaturesAndLabels()
         scores = crossV.cross_val_score(model, features, labels, cv=10, scoring='accuracy')
 
+        fileWriter.write("Acurácia dos 10 folds:\n")
+
         print("Acurácia dos 10 folds:")
         for score in scores:
+            fileWriter.write(str(score) + "\n")
             print(score)
+
+        fileWriter.write("Média das Acurácias %s" % scores.mean() + "\n")
+        fileWriter.write("Desvio Padrão das Acurácias %s" % scores.std() + "\n")
         print("Média das Acurácias %s" % scores.mean())
         print("Desvio Padrão das Acurácias %s" % scores.std())
 
-    def printAccruraciesAndStds(self):
+    def printAccruraciesAndStds(self, fileWriter):
         decisionTreeModel = DecisionTreeClassifier()
 
         knnModel1 = KNeighborsClassifier(n_neighbors=1)
         knnModel3 = KNeighborsClassifier(n_neighbors=3)
+        knnModel4 = KNeighborsClassifier(n_neighbors=4)
         knnModel5 = KNeighborsClassifier(n_neighbors=5)
 
         naiveBayesModel = GaussianNB()
 
+        fileWriter.write("\nDecision Tree Model\n\n")
         print("Decision Tree Model")
-        self.accuracyAndStd(decisionTreeModel)
+        self.accuracyAndStd(decisionTreeModel, fileWriter)
+
+        fileWriter.write("\nKNN Model K = 1\n\n")
         print("\nKNN Model K = 1")
-        self.accuracyAndStd(knnModel1)
+        self.accuracyAndStd(knnModel1, fileWriter)
+
+        fileWriter.write("\nKNN Model K = 3\n\n")
         print("\nKNN Model K = 3")
-        self.accuracyAndStd(knnModel3)
+        self.accuracyAndStd(knnModel3, fileWriter)
+
+        fileWriter.write("\nKNN Model K = 4\n\n")
+        print("\nKNN Model K = 4")
+        self.accuracyAndStd(knnModel4, fileWriter)
+
+        fileWriter.write("\nKNN Model K = 5\n\n")
         print("\nKNN Model K = 5")
-        self.accuracyAndStd(knnModel5)
+        self.accuracyAndStd(knnModel5, fileWriter)
+
+        fileWriter.write("\nNaive Bayes Model\n\n")
         print("\nNaive Bayes Model")
-        self.accuracyAndStd(naiveBayesModel)
+        self.accuracyAndStd(naiveBayesModel, fileWriter)
