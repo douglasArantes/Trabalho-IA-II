5fbcbf13238a6d227790b1005adc2b1cf3affb99
douglasArantes <douglas_arantes@outlook.com>
2015-06-09 23:52:14 -0400

    Salvando resultados em arquivos separados para cada dataset

--- classificador/Teste.py
+++ classificador/Teste.py
@@ -14,58 +14,85 @@ from classificador.modelos.WineClassifier import WineClassifier
 
 class Teste:
 
-    outputFile = os.path.join(os.getcwd(), "output.txt")
-    fileWriter = codecs.open(outputFile, 'w', "utf-8")
+    balanceScaleFile = os.path.join(os.getcwd(), "balanceScale_output.txt")
+    balanceScaleWriter = codecs.open(balanceScaleFile, 'w', "utf-8")
+
+    glassFile = os.path.join(os.getcwd(), "glass_output.txt")
+    glassWriter = codecs.open(glassFile, 'w', "utf-8")
+
+    habermanFile = os.path.join(os.getcwd(), "haberman_output.txt")
+    habermanWriter = codecs.open(habermanFile, 'w', "utf-8")
+
+    imgSegFile = os.path.join(os.getcwd(), "imgSegment_output.txt")
+    imgSegWriter = codecs.open(imgSegFile, 'w', "utf-8")
+
+    ionosphereFile = os.path.join(os.getcwd(), "ionosphere_output.txt")
+    ionosphereWriter = codecs.open(ionosphereFile, 'w', "utf-8")
+
+    irisFile = os.path.join(os.getcwd(), "iris_output.txt")
+    irisWriter = codecs.open(irisFile, 'w', "utf-8")
+
+    primaIndiansFile = os.path.join(os.getcwd(), "primaIndians_output.txt")
+    primaIndiansWriter = codecs.open(primaIndiansFile, 'w', "utf-8")
+
+    sonarAllFile = os.path.join(os.getcwd(), "sonarAll_output.txt")
+    sonarAllWriter = codecs.open(sonarAllFile, 'w', "utf-8")
+
+    spamBaseFile = os.path.join(os.getcwd(), "spamBase_output.txt")
+    spamBaseWriter = codecs.open(spamBaseFile, 'w', "utf-8")
+
+    wineFile = os.path.join(os.getcwd(), "wine_output.txt")
+    wineWriter = codecs.open(wineFile, 'w', "utf-8")
 
 
     if __name__ == "__main__":
 
-        fileWriter.write("-------- Dataset Iris --------")
+        irisWriter.write("-------- Dataset Iris --------")
         print("-------- Dataset Iris --------\n")
-        IrisClassifier().printAccruraciesAndStds(fileWriter)
+        IrisClassifier().printAccruraciesAndStds(irisWriter)
         print()
 
-        fileWriter.write("-------- Dataset Prima Indians Diabetes --------")
+        primaIndiansWriter.write("-------- Dataset Prima Indians Diabetes --------")
         print("-------- Dataset Prima Indians Diabetes --------\n")
-        DiabetesClassifier().printAccruraciesAndStds(fileWriter)
+        DiabetesClassifier().printAccruraciesAndStds(primaIndiansWriter)
         print()
 
-        fileWriter.write("-------- Dataset Balance Scale --------")
+        balanceScaleWriter.write("-------- Dataset Balance Scale --------")
         print("-------- Dataset Balance Scale --------\n")
-        BalanceScaleClassifier().printAccruraciesAndStds(fileWriter)
+        BalanceScaleClassifier().printAccruraciesAndStds(balanceScaleWriter)
         print()
 
-        fileWriter.write("-------- Dataset Sonar All --------")
+        sonarAllWriter.write("-------- Dataset Sonar All --------")
         print("-------- Dataset Sonar All --------\n")
-        SonarClassifier().printAccruraciesAndStds(fileWriter)
+        SonarClassifier().printAccruraciesAndStds(sonarAllWriter)
         print()
 
-        fileWriter.write("-------- Dataset Glass  --------")
+        glassWriter.write("-------- Dataset Glass  --------")
         print("-------- Dataset Glass  --------\n")
-        GlassClassifier().printAccruraciesAndStds(fileWriter)
+        GlassClassifier().printAccruraciesAndStds(glassWriter)
         print()
 
-        fileWriter.write("-------- Dataset Haberman  --------")
+        habermanWriter.write("-------- Dataset Haberman  --------")
         print("-------- Dataset Haberman  --------\n")
-        HabermanClassifier().printAccruraciesAndStds(fileWriter)
+        HabermanClassifier().printAccruraciesAndStds(habermanWriter)
         print()
 
-        fileWriter.write("-------- Dataset Ionsphere --------")
+        ionosphereWriter.write("-------- Dataset Ionsphere --------")
         print("-------- Dataset Ionsphere --------\n")
-        IonosphereClassifier().printAccruraciesAndStds(fileWriter)
+        IonosphereClassifier().printAccruraciesAndStds(ionosphereWriter)
         print()
 
-        fileWriter.write("-------- Dataset Spams --------")
+        spamBaseWriter.write("-------- Dataset Spams --------")
         print("-------- Dataset Spams --------\n")
-        SpamClassifier().printAccruraciesAndStds(fileWriter)
+        SpamClassifier().printAccruraciesAndStds(spamBaseWriter)
         print()
 
-        fileWriter.write("-------- Dataset Image Segmentation --------")
+        imgSegWriter.write("-------- Dataset Image Segmentation --------")
         print("-------- Dataset Image Segmentation --------\n")
-        ImageSegmentationClassifier().printAccruraciesAndStds(fileWriter)
+        ImageSegmentationClassifier().printAccruraciesAndStds(imgSegWriter)
         print()
 
-        fileWriter.write("-------- Dataset Wine --------")
+        wineWriter.write("-------- Dataset Wine --------")
         print("-------- Dataset Wine --------\n")
-        WineClassifier().printAccruraciesAndStds(fileWriter)
+        WineClassifier().printAccruraciesAndStds(wineWriter)
         print()
